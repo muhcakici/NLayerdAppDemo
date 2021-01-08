e624fdb96aeb9d39e103e4aa84d31c9dfc1f6dd8
M. Arda ÇAKICI <62748759+ardacakici@users.noreply.github.com>
2021-01-08 21:33:13 +0300

    Add project files.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Northwind.Business.Abstract;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concrete;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Nortwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Northwind.DataAccess/Concrete/NHibernite/NhProductDal.cs
@@ -0,0 +1,42 @@
+﻿using Northwind.DataAccess.Abstract;
+using Nortwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete.NHibernite
+{
+    public class NhProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            
+        }
+
+        public void Delete(Product product)
+        {
+            
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            return new List<Product>()
+            {
+                new Product(){ CategoryId=1, ProductId=3, ProductName="PC", UnitPrice=3000, UnitsInStock=7, QuantityPerUnit="36 Boxes"}
+            };
+        }
+
+        public void Update(Product product)
+        {
+            
+        }
+    }
+}
