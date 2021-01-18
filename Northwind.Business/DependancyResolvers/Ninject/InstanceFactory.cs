69c8f1417f04111e9c76fc3df8fd4dae78c5de43
M. Arda ÇAKICI <62748759+ardacakici@users.noreply.github.com>
2021-01-18 18:59:46 +0300

    BugFixed

---
+++ Northwind.Business/DependancyResolvers/Ninject/InstanceFactory.cs
@@ -0,0 +1,19 @@
+﻿using Ninject;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.DependancyResolvers.Ninject
+{
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var karnel = new StandardKernel(new BusinessModule());
+            return karnel.Get<T>();
+        }
+    }
+}
--- Northwind.WebFormsUI/Form1.cs
+++ Northwind.WebFormsUI/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using Northwind.Business.Abstract;
 using Northwind.Business.Concrete;
+using Northwind.Business.DependancyResolvers.Ninject;
 using Northwind.DataAccess.Concrete.EntityFramework;
 using Northwind.DataAccess.Concrete.NHibernite;
+using Nortwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,11 +21,144 @@ namespace Northwind.WebFormsUI
         public Form1()
         {
             InitializeComponent();
+            _productService = InstanceFactory.GetInstance<IProductService>();
+            _categoryService = InstanceFactory.GetInstance<ICategoryService>();
         }
-        IProductService _productService= new ProductManager(new EfProductDal());
+        private IProductService _productService;
+        private ICategoryService _categoryService;
         private void Form1_Load(object sender, EventArgs e)
         {
-            dgwProduct.DataSource = _productService.GetAll();           
+            LoadProducts();
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            cbxCategory.DataSource = _categoryService.GetAll();
+            cbxCategory.DisplayMember = "CategoryName";
+            cbxCategory.ValueMember = "CategoryId";
+
+            cbxCategoryId.DataSource = _categoryService.GetAll();
+            cbxCategoryId.DisplayMember = "CategoryName";
+            cbxCategoryId.ValueMember = "CategoryId";
+
+            cbxCategoryIdUpdate.DataSource = _categoryService.GetAll();
+            cbxCategoryIdUpdate.DisplayMember = "CategoryName";
+            cbxCategoryIdUpdate.ValueMember = "CategoryId";
+        }
+
+        private void LoadProducts()
+        {
+            dgwProduct.DataSource = _productService.GetAll();
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dgwProduct.DataSource = _productService.GetProductByCategory(Convert.ToInt32(cbxCategory.SelectedValue));
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            
+        }
+
+        private void tbxProductName_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tbxProductName.Text))
+            {
+                dgwProduct.DataSource = _productService.GetProductsByProductName(tbxProductName.Text);
+            }
+            else
+            {
+                LoadProducts();
+            }
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _productService.Add(new Product
+                {
+                    CategoryId = Convert.ToInt32(cbxCategoryId.SelectedValue),
+                    ProductName = tbxPruductName2.Text,
+                    QuantityPerUnit = tbxQuantityPerUnit.Text,
+                    UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                    UnitsInStock = Convert.ToInt16(tbxStock.Text)
+
+                });
+                MessageBox.Show("Ürün Başarıyla Eklendi.");
+                LoadProducts();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _productService.Update(new Product
+                {
+                    ProductId = Convert.ToInt32(dgwProduct.CurrentRow.Cells[0].Value),
+                    ProductName = tbxProductNameUpdate.Text,
+                    CategoryId = Convert.ToInt32(cbxCategoryIdUpdate.SelectedValue),
+                    UnitsInStock = Convert.ToInt16(tbxUnitStockUpdate.Text),
+                    QuantityPerUnit = tbxQuantityPerUnitUpdate.Text,
+                    UnitPrice = Convert.ToDecimal(tbxUnitPriceUpdate.Text)
+
+                });
+                MessageBox.Show("Ürün Başarıyla Güncellendi.");
+                LoadProducts();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void dgwProduct_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var Row = dgwProduct.CurrentRow;
+            tbxProductNameUpdate.Text = Row.Cells[1].Value.ToString();
+            cbxCategoryIdUpdate.SelectedValue = Row.Cells[2].Value;
+            tbxUnitPriceUpdate.Text = Row.Cells[3].Value.ToString();
+            tbxQuantityPerUnitUpdate.Text = Row.Cells[4].Value.ToString();
+            tbxUnitStockUpdate.Text = Row.Cells[5].Value.ToString();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+            if(dgwProduct.CurrentRow!=null)
+                try
+                {
+                    _productService.Delete(new Product
+                    {
+                        ProductId = Convert.ToInt32(dgwProduct.CurrentRow.Cells[0].Value)
+                    });
+                    MessageBox.Show("Ürün Başarıyla Silindi!");
+                    LoadProducts();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+
+            
         }
     }
 }
